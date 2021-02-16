6af2b769988454256ebf124fc0df70d14bfac2f1
zjschulz <zjschulz@gmail.com>
2021-02-16 00:46:48 +0000

    test port for email fetch request

--- react-ui/src/components/Email.js
+++ react-ui/src/components/Email.js
@@ -55,6 +55,7 @@ class Email extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         const PORT = process.env.PORT || 5000;
+        console.log(POST)
         fetch(PORT + '/send', {
             method: "POST",
             body: JSON.stringify(this.state),
